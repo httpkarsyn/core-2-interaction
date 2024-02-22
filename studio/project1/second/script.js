bd252388ba2ca7a619eba5cf0bcccc9c3ebcea83
httpkarsyn <146144288+httpkarsyn@users.noreply.github.com>
2024-02-21 23:05:44 -0500

    final

--- studio/project1/second/script.js
+++ studio/project1/second/script.js
@@ -1,30 +1,24 @@
 let body = document.body,
-  scroll_counter = 0,
-  auto_scroll_kicked = false,
-  scroller; // Declare scroller globally
+    scroll_counter = 0,
+    auto_scroll_kicked = false,
+    scroller; // Declare scroller globally
 
 // Start auto-scrolling when the page loads
 window.addEventListener("load", startAutoScrolling);
 
 function startAutoScrolling() {
-  auto_scroll_kicked = true;
-  // Increase the scroll counter
-  scroll_counter += 1.2;
-  // Set the scroll position
-  document.documentElement.scrollTop = scroll_counter;
-  // Request the next animation frame
-  scroller = window.requestAnimationFrame(startAutoScrolling);
+    auto_scroll_kicked = true;
+    // Increase the scroll counter
+    scroll_counter += 1.4;
+    // Set the scroll position
+    document.documentElement.scrollTop = scroll_counter;
+    // Request the next animation frame
+    scroller = window.requestAnimationFrame(startAutoScrolling);
 
-  // Cancel the animation frame when the scrolling reaches the end
-  if (scroll_counter >= body.offsetHeight) {
-    window.cancelAnimationFrame(scroller);
-  }
+    // Cancel the animation frame when the scrolling reaches the end
+    if (scroll_counter >= body.offsetHeight) {
+        window.cancelAnimationFrame(scroller);
+    }
 }
 
-// Stop auto-scrolling when the user scrolls manually
-/*window.addEventListener("wheel", () => {
-  if (auto_scroll_kicked) {
-    window.cancelAnimationFrame(scroller);
-    auto_scroll_kicked = false;
-  }
-});*/
+
