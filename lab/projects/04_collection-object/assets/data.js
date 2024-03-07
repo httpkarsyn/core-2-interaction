52baa8d2127e2710bb10b447544b8ed5cd88e6ba
httpkarsyn <146144288+httpkarsyn@users.noreply.github.com>
2024-03-07 14:07:51 -0500

    update

---
+++ lab/projects/04_collection-object/assets/data.js
@@ -0,0 +1,122 @@
+// An array of objects representing my collection:
+var data = [
+    {
+        trash: 'glove',
+        material: 'cloth & rubber',
+        price: 100,
+    },
+
+    {
+        trash: 'thank you bag',
+        material: 'plastic',
+        price: 10,
+    },
+
+    {
+        trash: 'two coffee cups',
+        material: 'cardboard & plastic',
+        price: 40,
+    },
+
+    {
+        trash: 'cheeto bag',
+        material: 'plastic',
+        price: 100,
+    },
+
+    {
+        trash: 'milk caton',
+        material: 'thin plastic',
+        price: 5,
+    },
+
+    {
+        trash: 'can in bag',
+        material: 'aluminum & plastic',
+        price: 210,
+    },
+
+    {
+        trash: 'togo box',
+        material: 'cardboard',
+        price: .6,
+    },
+
+    {
+        trash: 'can of joes',
+        material: 'aluminum',
+        price: 200,
+    },
+
+    {
+        trash: 'butterfingers',
+        material: 'plastic',
+        price: 100,
+    },
+
+    {
+        trash: 'coffee cup',
+        material: 'styrofoam',
+        price: 1000,
+    },
+
+    {
+        trash: 'lays chips',
+        material: 'plastic',
+        price: 100,
+    },
+
+    {
+        trash: 'forks',
+        material: 'polypropylene plastic',
+        price: 100,
+    },
+
+    {
+        trash: 'canada dry bottle',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+    },
+
+    {
+        trash: 'arizona bottle',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+    },
+
+    {
+        trash: 'sneakers',
+        material: 'synthetic fabrics',
+        price: 200,
+    },
+
+    {
+        trash: 'old apple',
+        material: 'apple',
+        price: .1 ,
+    },
+
+    {
+        trash: 'water jug',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+    },
+
+    {
+        trash: 'pizza',
+        material: 'pizza',
+        price: .3,
+    },
+
+    {
+        trash: 'pizza box',
+        material: 'cardboard',
+        price: 1,
+    },
+
+    {
+        trash: 'water bottle',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+    },
+];
