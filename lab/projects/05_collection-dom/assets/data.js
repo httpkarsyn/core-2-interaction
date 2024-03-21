a1250e181dd31abfede6c5115bbf0e7d65280e7d
httpkarsyn <146144288+httpkarsyn@users.noreply.github.com>
2024-03-21 15:56:21 -0400

    project 5 lab

---
+++ lab/projects/05_collection-dom/assets/data.js
@@ -0,0 +1,140 @@
+// An array of objects representing my collection:
+var data = [
+    {
+        trash: 'glove',
+        material: 'cloth & rubber',
+        price: 100,
+        image: 'glove.png'
+    },
+
+    {
+        trash: 'thank you bag',
+        material: 'plastic',
+        price: 10,
+        image: 'thankyoubag.png'
+    },
+
+    {
+        trash: 'two coffee cups',
+        material: 'cardboard & plastic',
+        price: 40,
+        image: 'coffees.png'
+    },
+
+    {
+        trash: 'cheeto bag',
+        material: 'plastic',
+        price: 100,
+        image: 'cheetobag.png'
+    },
+
+    {
+        trash: 'milk carton',
+        material: 'thin plastic',
+        price: 5,
+        image: 'milk.png'
+    },
+
+    {
+        trash: 'can in bag',
+        material: 'aluminum & plastic',
+        price: 210,
+        image: 'beercan.png'
+    },
+
+    {
+        trash: 'togo box',
+        material: 'cardboard',
+        price: .6,
+        image: 'foodbox.png'
+    },
+
+    {
+        trash: 'can of joes',
+        material: 'aluminum',
+        price: 200,
+        image: 'joes.png'
+    },
+
+    {
+        trash: 'butterfingers',
+        material: 'plastic',
+        price: 100,
+        image: 'candy.png'
+    },
+
+    {
+        trash: 'coffee cup',
+        material: 'styrofoam',
+        price: 1000,
+        image: 'styro.png'
+    },
+
+    {
+        trash: 'lays chips',
+        material: 'plastic',
+        price: 100,
+        image: 'lays.png'
+    },
+
+    {
+        trash: 'forks',
+        material: 'polypropylene plastic',
+        price: 100,
+        image: 'fork.png'
+    },
+
+    {
+        trash: 'canada dry bottle',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+        image: 'canadadry.png'
+    },
+
+    {
+        trash: 'arizona bottle',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+        image: 'arizonatea.png'
+
+    },
+
+    {
+        trash: 'sneakers',
+        material: 'synthetic fabrics',
+        price: 200,
+        image: 'shoes.png'
+    },
+
+    {
+        trash: 'old apple',
+        material: 'apple',
+        price: .1 ,
+        image: 'apple.png'
+    },
+
+    {
+        trash: 'water jug',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+        
+    },
+
+    {
+        trash: 'pizza',
+        material: 'pizza',
+        price: .3,
+    },
+
+    {
+        trash: 'pizza box',
+        material: 'cardboard',
+        price: 1,
+    },
+
+    {
+        trash: 'water bottle',
+        material: 'polyethylene terephthalate plastic',
+        price: 400,
+    },
+];
