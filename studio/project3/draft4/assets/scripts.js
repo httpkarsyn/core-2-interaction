6e61b985a6bc6a3d5f05134efac57ab66b47b1a3
httpkarsyn <146144288+httpkarsyn@users.noreply.github.com>
2024-05-03 10:49:37 -0400

    ugh

--- studio/project3/draft4/assets/scripts.js
+++ studio/project3/draft4/assets/scripts.js
@@ -1,76 +1,97 @@
+// Rendering countdown timer
 (function () {
-      const second = 1000,
-            minute = second * 60,
-            hour = minute * 60,
-            day = hour * 24;
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-      // January 1st, 2030
-      const countDown = new Date("January 1, 2030 00:00:00").getTime();
+    // January 1st, 2030
+    const countDown = new Date("January 1, 2030 00:00:00").getTime();
 
-      const x = setInterval(function () {
+    const x = setInterval(function () {
+        const now = new Date().getTime(),
+            distance = countDown - now;
 
-            const now = new Date().getTime(),
-                  distance = countDown - now;
-
-            document.getElementById("days").innerText = Math.floor(distance / (day)).toLocaleString();
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-
-      }, 0);
+        document.getElementById("days").innerText = Math.floor(distance / (day)).toLocaleString();
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+    }, 0);
 })();
 
-
-var key = 'e92c3c9cd77dad7b1b1d40b741adca16'; // api key
-
+// Fetching and rendering data from APIs
 var lat = 40.73;
 var lng = -74.00;
 
-var URL_1 = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${lng}&hourly=ozone&forecast_days=1`;
-var URL_2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=e92c3c9cd77dad7b1b1d40b741adca16&units=imperial`;
-var URL_3 = `https://climate-api.open-meteo.com/v1/climate?latitude=${lat}&longitude=${lng}&start_date=1950-01-01&end_date=2050-12-31&daily=temperature_2m_max`
+var URL_1 = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=35.3733&longitude=-119.0187&hourly=dust&forecast_days=1`;
+var URL_2 = `https://api.open-meteo.com/v1/forecast?latitude=-41.2866&longitude=174.7756&hourly=wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&forecast_days=1`;
+var URL_4 = `https://api.open-meteo.com/v1/forecast?latitude=16.4333&longitude=-93.7953&hourly=temperature_2m&temperature_unit=fahrenheit&forecast_days=1`;
+var URL_5 = `https://api.open-meteo.com/v1/forecast?latitude=80.216&longitude=-76.1238&hourly=snowfall&temperature_unit=fahrenheit&forecast_days=1`;
+var URL_6 = `https://api.open-meteo.com/v1/forecast?latitude=25.2973&longitude=91.5827&hourly=rain&temperature_unit=fahrenheit&wind_speed_unit=mph&forecast_days=1`
+
 fetch(URL_1)
-      .then((response) => {
-            return response.json();
-      })
-      .then((data) => {
-            renderAirQuality(data);
-      })
+    .then((response) => response.json())
+    .then((data) => renderDust(data));
 
 fetch(URL_2)
-      .then((response) => {
-            return response.json();
-      })
-      .then((data) => {
-            renderWeather(data);
-      })
+    .then((response) => response.json())
+    .then((data) => renderWindSpeed(data));
 
-      fetch(URL_3)
-      .then((response) => {
-            return response.json();
-      })
-      .then((data) => {
-            renderClimate(data);
-      })
+fetch(URL_4)
+    .then((response) => response.json())
+    .then((data) => renderTemperature(data));
 
-function renderAirQuality(data) {
-      console.log(data);
+fetch(URL_5)
+    .then((response) => response.json())
+    .then((data) => renderSnowfall(data));
 
-      var elevationElement = document.querySelector('.api-overlay[data-property="elevation"]');
-     elevationElement.innerHTML = data.elevation;
+fetch(URL_6)
+    .then((response) => response.json())
+    .then((data) => renderRain(data));
+
+
+function renderDust(data) {
+    console.log(data);
+    var dustElement = document.querySelector('.api-overlay[data-property="dust"]');
+    dustElement.innerHTML = data.hourly.dust[hour];
 }
 
-function renderWeather(data) {
-      console.log(data);
-
-      var windSpeedElement = document.querySelector('.api-overlay[data-property="wind-speed"]');
-      windSpeedElement.innerHTML = data.wind.speed;
+function renderWindSpeed(data) {
+    console.log(data);
+    var windSpeedElement = document.querySelector('.api-overlay[data-property="wind-speed"]');
+    windSpeedElement.innerHTML = data.hourly.windSpeed_10m[hour] + "&nbsp;MPH";
 }
 
-function renderClimate(data) {
-      console.log(data);
+function renderTemperature(data) {
+    console.log(data);
+    var dateObject = new Date();
+    var hour = dateObject.getHours();
+    var temperatureElement = document.querySelector('.api-overlay[data-property="temperature"]');
+    temperatureElement.innerHTML = data.hourly.temperature_2m[hour] + "&nbsp;F";
+    var temperatureElement = document.querySelector('.api-overlay[data-property="temperature"]');
+    temperatureElement.innerHTML = temperature + "&nbsp;F";
+    
+//     bar
 
-      var maximumTemperatureElement = document.querySelector('.api-overlay[data-property="maximum-temperature"]');
-      maximumTemperatureElement.innerHTML = data.maximum.temperature;
+    var maxTemperature = 122; 
+    var barWidth = (temperature / maxTemperature) * 100; 
+    var barElement = document.querySelector('.bar');
+    barElement.style.width = barWidth + "%";
 }
+
+
+function renderSnowfall(data) {
+    console.log(data);
+    var dateObject = new Date();
+    var hour = dateObject.getHours();
+    var snowfallElement = document.querySelector('.api-overlay[data-property="snowfall"]');
+    snowfallElement.innerHTML = data.hourly.snowfall[hour] + "&nbsp;IN";
+}
+
+function renderRain(data) {
+      console.log(data);
+      var dateObject = new Date();
+      var hour = dateObject.getHours();
+      var rainElement = document.querySelector('.api-overlay[data-property="rain"]');
+      rainElement.innerHTML = data.hourly.rain [hour] + "&nbsp;IN";
+  }
