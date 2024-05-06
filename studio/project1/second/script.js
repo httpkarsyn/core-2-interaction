39a0f70cf50a42c913864ef8039a206b76318617
httpkarsyn <146144288+httpkarsyn@users.noreply.github.com>
2024-05-05 21:24:31 -0400

    p1 speed update

--- studio/project1/second/script.js
+++ studio/project1/second/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", startAutoScrolling);
 
 function startAutoScrolling() {
     auto_scroll_started = true;
-    scroll_counter += 1.2; // Adjust increment as needed
+    scroll_counter += .8; 
     window.scrollTo(0, scroll_counter);
 
     scroller = window.requestAnimationFrame(startAutoScrolling);
